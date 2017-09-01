ce2de64c0ca926cab810c698b70444495a5e0336
Daniel Hiller <Daniel_Hiller@intuit.com>
2017-09-01 16:49:04 +0200

    Add POST

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Spring DeferredResult Timeout Test
 
+## GET
+
 Demonstrates the timeout usage:
 
 ```
@@ -8,10 +10,28 @@ curl -v 'http://localhost:8080/timeout?waitTime=1500'
 
 will create a HTTP/1.1 503 Service Unavailable
 
-while 
+while
 
 ```
 curl -v 'http://localhost:8080/timeout?waitTime=500'
+```
+
+will create a HTTP/1.1 200 OK
+
+## POST
+
+Demonstrates the timeout usage:
+
+```
+curl -v -H'Content-Type: application/json' -d '1500' 'http://localhost:8080/timeout'
+```
+
+will create a HTTP/1.1 503 Service Unavailable
+
+while
+
+```
+curl -v -H'Content-Type: application/json' -d '500' 'http://localhost:8080/timeout'
 ```
 
 will create a HTTP/1.1 200 OK
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -3,6 +3,8 @@ package hello;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.async.DeferredResult;
 
@@ -13,7 +15,17 @@ public class GreetingController {
 
     @GetMapping("/timeout")
     public DeferredResult<ResponseEntity<?>> timeout(@RequestParam(name="waitTime") final long waitTime) {
-        final DeferredResult<ResponseEntity<?>> responseEntityDeferredResult = new DeferredResult<>(1000L);
+        return createDeferredResultWithTimeoutAndWaitTime(waitTime, 1000L);
+    }
+
+    @PostMapping("/timeout")
+    public DeferredResult<ResponseEntity<?>> timeoutPost(@RequestBody final long waitTime) {
+        return createDeferredResultWithTimeoutAndWaitTime(waitTime, 1000L);
+    }
+
+    private DeferredResult<ResponseEntity<?>> createDeferredResultWithTimeoutAndWaitTime(
+            @RequestParam(name = "waitTime") long waitTime, long timeoutAfter) {
+        final DeferredResult<ResponseEntity<?>> responseEntityDeferredResult = new DeferredResult<>(timeoutAfter);
         CompletableFuture.runAsync(() -> {
             if (waitTime > 0) {
                 try {
